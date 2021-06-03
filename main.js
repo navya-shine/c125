7d1d94c8ea94ea538fc7cd808b7125ae781c4f8b
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-06-03 16:22:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightwristX = 0;
+leftwristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,470);
+    canvas.position(560,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw()
+{
+    background('#5D5C5C');
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX,noseY,difference);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is Initialised ');
+}
+function gotPoses(results)
+{
+    if(results>0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +"noseY = " + noseY);
+
+        leftwristX = results[0].pose.leftwrist.x;
+        rightwristX = results[0].pose.rightwrist.x;
+        difference = floor( leftwristX - rightwrist )
+        console.log("leftwrist = " + leftwristX + "rightwrist = " + rightwristX + "difference = " + difference);
+    }
+}
